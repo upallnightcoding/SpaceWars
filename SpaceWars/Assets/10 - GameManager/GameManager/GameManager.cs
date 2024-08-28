265d72ab80cc6670813ad81b6bb9119805570c60
upallnightcoding <upallnightcoding@gmail.com>
2024-08-28 00:08:38 -0400

    Starting Game

--- SpaceWars/Assets/10 - GameManager/GameManager/GameManager.cs
+++ SpaceWars/Assets/10 - GameManager/GameManager/GameManager.cs
@@ -125,7 +125,7 @@ public class GameManager : MonoBehaviour
         }
 
         uiCntrl.HideEngageCountDown();
-        //enemyManager.GameLevel(2, fighter);
+        enemyManager.GameLevel(2, fighter);
         cameraCntrl.StartGame(fighter.transform);
     }
 
